70ae9a3de3127619994ddd55eff02af0eea725a1
Dhruv <dhruvbhagat98@gmail.com>
2025-01-06 22:43:14 +0530

    allowed public access

--- server/src/routes/formSubmitRouter.js
+++ server/src/routes/formSubmitRouter.js
@@ -1,18 +1,11 @@
 import { Router } from "express";
 
 import { restrictUpdateFields } from "../middlewares/restrictFieldsMiddleware.js";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
-import {
-  createFormSubmit,
-  getFormSubmit,
-  getFormSubmits,
-} from "../controllers/formSubmitController.js";
+import { createFormSubmit } from "../controllers/formSubmitController.js";
 import validatorMiddleware from "../middlewares/validatorMiddleware.js";
 import { createFormSubmitValidation } from "../validations/formSubmitValidation.js";
 
 const formSubmitRouter = Router();
-
-formSubmitRouter.use(authMiddleware);
 
 formSubmitRouter.post(
   "",
